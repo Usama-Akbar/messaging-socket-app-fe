a02bc1254d9c3609ab5b315b58b718b8288f49d1
Usama Akbar <usamamehar435@gmail.com>
2024-07-07 19:12:04 +0500

    sockers loging

--- src/Pages/ChatPage.js
+++ src/Pages/ChatPage.js
@@ -26,6 +26,24 @@ const ChatPage = ({ token }) => {
     };
   }, []);
 
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+  
+    socket.on('error', (error) => {
+      console.error('Socket error:', error);
+    });
+  
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const decoded = JSON.parse(atob(token.split('.')[1]));
